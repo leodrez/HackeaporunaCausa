9c3d10e6ace64c22c26105aec694ba37972589c2
Luis <webdeveloperpr@gmail.com>
2017-04-01 18:25:21 -0400

    Added initial routes

---
+++ app/src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Router, browserHistory, IndexRoute } from 'react-router';
+import App from './pages/App';
+import Appointments from './pages/Appointments';
+import Institutions from './pages/Institutions';
+import Profile from './pages/Profile';
+
+/**
+ * Has access to the redux store. Useful for validation purposes
+ * @param store
+ */
+export default (store) => {
+  return (
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <Route path="/profile" component={Profile}/>
+        <Route path="/institution" component={Institutions}/>
+        <Route path="/appointment" component={Appointments}/>
+      </Route>
+    </Router>
+  );
+}
