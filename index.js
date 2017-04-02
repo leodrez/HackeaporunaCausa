8c516ef0661f92e9942d0f289f6bc53c82b780fa
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-04-02 10:21:52 -0400

    mlab

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const app = express()
 const mongoose = require('mongoose')
 const api = require('./routes')
 
-mongoose.connect('mongodb://localhost:27017/hackporunacausa', (err) => {
+let url
+if (process.env.NODE_ENV === 'production') {
+  url = `mongodb://hackporunacausa:12345678@ds147480.mlab.com:47480/hackporunacausa`
+} else {
+  url = 'mongodb://localhost:27017/hackporunacausa'
+}
+
+mongoose.connect(url, (err) => {
   if (err) console.log(`Connection to database failed, ${err}`)
 
   console.log(`Database connection established`)
